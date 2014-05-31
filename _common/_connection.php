6f47be40a41b59311307fbd2f87ccb917a4cb6f7
sanil <santonil2004@yahoo.com>
2014-05-31 20:08:00 +1000

    "local db credential saved"

--- _common/_connection.php
+++ _common/_connection.php
@@ -1,52 +1,58 @@
 <?php
-class Config
-{
+
+class Config {
+
     var $dbhost;
     var $dbuser;
     var $dbpass;
     var $dbname;
 
-    function Config()
-	{	
-		  $this->dbhost='localhost';
-        $this->dbuser='identiki';
-        $this->dbpass='id4$cTe';
-        $this->dbname='identikid';
-	}
+    function Config() {
+        if (isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+            $this->dbhost = 'localhost';
+            $this->dbuser = 'root';
+            $this->dbpass = '';
+            $this->dbname = 'identikid';
+        } else {
+            $this->dbhost = 'localhost';
+            $this->dbuser = 'identiki';
+            $this->dbpass = 'id4$cTe';
+            $this->dbname = 'identikid';
+        }
+    }
+
 }
 
-class DbConnect
-{
-	var $cfg;
-	var $conn;
+class DbConnect {
 
-	function DbConnect($cfg)
-	{
-		$this->cfg = $cfg;
-	}
+    var $cfg;
+    var $conn;
 
-	function connectDb()
-	{
-		$this->conn = mysql_connect($this->cfg->dbhost,$this->cfg->dbuser,$this->cfg->dbpass);
-		mysql_select_db($this->cfg->dbname,$this->conn) or die( 'Unable to select database:common MySQL');
-	}
+    function DbConnect($cfg) {
+        $this->cfg = $cfg;
+    }
 
-	function closeDb()
-	{
-		if(isset($this->conn))
-		{
-			@mysql_free_result($this->conn);
-			@mysql_close($this->conn);
-		}	
-	}  
+    function connectDb() {
+        $this->conn = mysql_connect($this->cfg->dbhost, $this->cfg->dbuser, $this->cfg->dbpass);
+        mysql_select_db($this->cfg->dbname, $this->conn) or die('Unable to select database:common MySQL');
+    }
+
+    function closeDb() {
+        if (isset($this->conn)) {
+            @mysql_free_result($this->conn);
+            @mysql_close($this->conn);
+        }
+    }
+
 }
 
+function linkme() {
 
-function linkme(){
+    $cfg = new Config();
 
-  $cfg = new Config();
-  $db = new DbConnect($cfg);
-  $db->connectDb();
-  
+    $db = new DbConnect($cfg);
+
+    $db->connectDb();
 }
+
 ?>
