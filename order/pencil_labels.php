6180ad8f1392ad9c44bd950937fdbc89910eae1b
sanil <santonil2004@yahoo.com>
2014-06-26 18:42:09 +1000

    making live vinyl and mini vinyl

--- order/pencil_labels.php
+++ order/pencil_labels.php
@@ -1,31 +1,3 @@
 <table width="100%" ><tr align="center" ><td align="center" >
-
-<script language="JavaScript" type="text/javascript">
-<!--
-// Version check based upon the values entered above in "Globals"
-var hasReqestedVersion = DetectFlashVer(requiredMajorVersion, requiredMinorVersion, requiredRevision);
-// Check to see if the version meets the requirements for playback
-if (hasReqestedVersion) {
-	// if we've detected an acceptable version
-	// embed the Flash Content SWF when all tests are passed
-	AC_FL_RunContent(
-				"src", "images/order_pencil_labels",
-				"flashVars", "v=300904",
-				"width", "350",
-				"height", "420",
-				"wmode", "transparent",
-				"align", "middle",
-				"id", "order_pencil_labels",
-				"quality", "high",
-				"bgcolor", "#FFFFFF",
-				"name", "order_pencil_labels",
-				"allowScriptAccess","sameDomain",
-				"type", "application/x-shockwave-flash",
-				'codebase', 'http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab',
-				"pluginspage", "http://www.adobe.com/go/getflashplayer"
-	);
-}
-// -->
-</script>
-
+<iframe src="/_designer/vinyls_mini.php" height="1200"width="100%" frameBorder="0"></iframe>
 </td></tr></table>
