bd9f53f8cd61a75be4ccd9406efe53f2716c4708
sanil <santonil2004@yahoo.com>
2014-06-26 18:51:25 +1000

    making mini vinul and pencil label live

--- order/pencil_labels.php
+++ order/pencil_labels.php
@@ -1,3 +1,3 @@
 <table width="100%" ><tr align="center" ><td align="center" >
-<iframe src="/_designer/vinyls_mini.php" height="1200"width="100%" frameBorder="0"></iframe>
+<iframe src="/_designer/pencil_labels.php" height="720"width="100%" frameBorder="0"></iframe>
 </td></tr></table>
