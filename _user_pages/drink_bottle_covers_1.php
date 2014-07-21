8fab4f8c669d2ad754c8d664ad963c23decee948
centos <santonil2004@hotmail.com>
2014-07-21 06:18:03 -0400

    dding page to git:wq

---
+++ _user_pages/drink_bottle_covers_1.php
@@ -0,0 +1,26 @@
+<FONT size=6>&nbsp;</FONT> 
+<P align=center><FONT size=5><FONT face=Verdana>INSULATED AND PERSONALISED</FONT><FONT face=Verdana>&nbsp;DRINK BOTTLE COVERS </FONT></FONT></P>
+<P align=center><FONT size=4 face=Verdana>Make Their Drinks Look and Stay Cool!</FONT></P>
+<P>
+<TABLE style="HEIGHT: 1201px; WIDTH: 863px" height=1201 cellSpacing=10 cellPadding=10 width=863 border=0>
+<TBODY>
+<TR>
+<TD vAlign=top rowSpan=3 colSpan=3>
+<P align=center><FONT size=5 face=Verdana><IMG src="http://identikid.com.au/images/bg/Example.png"></FONT></P>
+<P align=center><FONT size=5 face=Verdana>What does every kid want and Need?</FONT></P>
+<P align=center><FONT face=Verdana>A personalised , <STRONG>insulated</STRONG> and highly Practical Drink Bottle Cover.&nbsp; </FONT></P>
+<P align=center><FONT face=Verdana>Made from 5mm Neoprene with clip for bag or lanyard. </FONT><FONT face=Verdana>Fits&nbsp;most makes of regular barrel shaped bottles </FONT><FONT face=Verdana>with stretch and adjustable fastener at opening to allow for smaller bottles.</FONT></P>
+<P align=center><FONT face=Verdana>School Drink Bottles and Water Bottles. No more lost property. </FONT><FONT face=Verdana>Sized&nbsp;6-8 cm diameter base and 180mm height it will accomodate smaller and larger sized bottles. </FONT></P>
+<P align=center><FONT face=Verdana>Insulating. Identifiable. Choose Black , Pink or Blue Material . </FONT></P>
+<P align=center><FONT face=Verdana>Add your Design. Add your Name. The most practical gift on the planet.</FONT></P>
+<P align=center><FONT color=#ff0056 size=4>ALLOW 5-7 BUSINESS DAYS FOR YOUR SPECIAL GIFT TO BE DISPATCHED.</FONT></P>
+<P align=center><A href="http://identikid.com.au/Drink_Bottle_Covers/Bottle_Covers_Designs"><IMG style="HEIGHT: 56px; WIDTH: 299px" border=0 src="/images/bg/view-designs-button_1.gif" width=0 height=0></A></P>
+<P align=center><FONT face=Verdana><FONT size=4>PRICE&nbsp;$26.00&nbsp;</FONT></FONT></P>
+<P align=center><FONT face=Verdana><FONT color=#8181ff size=2>THIS ITEM CANNOT BE DISPATCHED <STRONG>IN LESS THEN 5 DAYS</STRONG> EVEN WHEN CHOOSING</FONT></P>
+<P align=center><FONT color=#8181ff size=2>&nbsp;THE EXPRESS DELIVERY DUE TO THE NATURE OF MANUFACTURE</FONT></P></FONT>
+<P align=center><FONT face=Verdana></FONT>&nbsp;</P>
+<P align=center>&nbsp;</P>
+<P align=center><FONT face=Verdana></FONT></P>
+<P align=center><FONT face=Verdana></FONT>&nbsp;</P></TD></TR>
+<TR><FONT face=Verdana></FONT></TR>
+<TR><FONT face=Verdana></FONT></TR></TBODY></TABLE></P>
