41c54d59c88f0c2577cd214247a01d1bfdd1c32b
centos <santonil2004@hotmail.com>
2014-08-10 10:55:20 -0400

    cms page updateed

--- _user_pages/great_gift_ideas.php
+++ _user_pages/great_gift_ideas.php
@@ -7,58 +7,56 @@
 <P align=center><FONT color=#252525 size=2>Postage of items displayed on Checkout.</FONT></P>
 <P align=center><FONT color=#252525 size=2></FONT>&nbsp;</P>
 <P align=center>
-<TABLE border=0 cellSpacing=4 cellPadding=4>
+<TABLE cellSpacing=4 cellPadding=4 border=0>
 <TBODY>
 <TR>
 <TD vAlign=top><FONT color=#0000ff size=4><A href="/pages/Wallets_with_WOW!">
 <P align=center><FONT color=#0000ff size=2>VELCRO WALLETS</FONT></P>
-<P align=center></A></FONT><A href="/pages/Wallets_with_WOW!"><FONT size=2><IMG style="WIDTH: 194px; HEIGHT: 103px" border=0 src="/images/bg/wallet-image-for-tracy.gif" width=0 height=0></FONT></A></P>
+<P align=center></A></FONT><A href="/pages/Wallets_with_WOW!"><FONT size=2><IMG style="HEIGHT: 103px; WIDTH: 194px" border=0 src="/images/bg/wallet-image-for-tracy.gif" width=0 height=0></FONT></A></P>
 <P align=center><FONT size=2></FONT>&nbsp;</P></TD>
 <TD><FONT color=#0000ff><A href="http://identikid.com.au/Coin_purses">
 <P align=center><FONT color=#0000ff size=2>COIN PURSES</FONT></P>
-<P align=center></A></FONT><FONT size=2>&nbsp;</FONT><A href="http://identikid.com.au/Coin_purses"><FONT size=2><IMG style="WIDTH: 216px; HEIGHT: 108px" border=0 src="/images/bg/coin-purse-image_4.gif" width=0 height=0></FONT></A></P>
+<P align=center></A></FONT><FONT size=2>&nbsp;</FONT><A href="http://identikid.com.au/Coin_purses"><FONT size=2><IMG style="HEIGHT: 108px; WIDTH: 216px" border=0 src="/images/bg/coin-purse-image_4.gif" width=0 height=0></FONT></A></P>
 <P align=center><FONT size=2></FONT>&nbsp;</P></TD>
 <TD vAlign=top><FONT color=#0000ff><A href="http://identikid.com.au/TOILETRY_BAGS_FOR_EVERYBODY">
 <P align=center><FONT color=#0000ff size=2>PERSONALISED TOILET/COSMETIC BAGS</FONT></P>
-<P align=center></A></FONT><A href="http://identikid.com.au/TOILETRY_BAGS_FOR_EVERYBODY"><FONT size=2><IMG style="WIDTH: 150px; HEIGHT: 122px" border=0 src="http://identikid.com.au/images/bg/Hibiscustoiletry_1.gif" width=0 height=0></FONT></A></P></TD></TR>
+<P align=center></A></FONT><A href="http://identikid.com.au/TOILETRY_BAGS_FOR_EVERYBODY"><FONT size=2><IMG style="HEIGHT: 122px; WIDTH: 150px" border=0 src="http://identikid.com.au/images/bg/Hibiscustoiletry_1.gif" width=0 height=0></FONT></A></P></TD></TR>
 <TR>
 <TD vAlign=top><FONT color=#0000ff><A href="/Lunchboxes">
-<P align=center><FONT color=#0000ff size=2>INSULATED LUNCHBOX</FONT></P>
-<P align=center></A></FONT><A href="http://identikid.com.au/Insulated_lunchboxes"><FONT size=2></FONT></A></P>
-<P align=center><FONT size=2><A href="http://identikid.com.au/Lunchboxes"><IMG style="WIDTH: 169px; HEIGHT: 156px" border=0 src="http://identikid.com.au/images/bg/PrincessLB_2.png" width=0 height=0></A></FONT></P></TD>
+<P align=center><FONT color=#0000ff size=2>BAGS</FONT></P>
+<P align=center></A></FONT><A href="http://identikid.com.au/Insulated_lunchboxes"><FONT size=2></FONT></A></P><FONT size=2>
+<P align=center><FONT size=2><A href="http://identikid.com.au/Bags"><IMG border=0 src="http://identikid.com.au/images/bg/BAGSWEBSITEIMAGE.png" width=229 height=147></A><A href="http://identikid.com.au/Bags"></A></FONT></P><A href="http://identikid.com.au/Lunchboxes"></A></FONT></TD>
 <TD vAlign=top><FONT color=#0000ff><A href="/Drink_Bottle_Covers">
 <P align=center><FONT color=#0000ff size=2>INSULATED BOTTLE COVERS</FONT></P>
 <P align=center></A></FONT><A href="/Drink_Bottle_Covers"><FONT size=2></FONT></A></P>
-<P align=center><FONT size=2><A href="http://identikid.com.au/Drink_Bottle_Covers"><IMG style="WIDTH: 169px; HEIGHT: 156px" border=0 src="http://identikid.com.au/images/bg/Example_1.png" width=0 height=0></A></FONT></P></TD>
-<TD vAlign=top><FONT color=#0000ff><A href="http://identikid.com.au/Personalised_Pet_Tags">
-<P align=center><FONT color=#0000ff size=2>PET TAGS</FONT></P></A></FONT>
-<P align=center><A href="http://identikid.com.au/Personalised_Pet_Tags"><FONT size=2><IMG style="WIDTH: 162px; HEIGHT: 126px" border=0 src="http://identikid.com.au/images/bg/Untitled-2_1.jpg" width=0 height=0></FONT></A></P>
-<P align=center>&nbsp;</P></TD></TR>
+<P align=center><FONT size=2><A href="http://identikid.com.au/Drink_Bottle_Covers"><IMG style="HEIGHT: 156px; WIDTH: 169px" border=0 src="http://identikid.com.au/images/bg/Example_1.png" width=0 height=0></A></FONT></P></TD>
+<TD vAlign=top><FONT color=#0000ff size=2><A href="http://identikid.com.au/Organic_Baby_Clothes">
+<P align=center>PERSONALISED BABY CLOTHES</P></A></FONT>
+<P align=center><A href="http://identikid.com.au/Personalised_Pet_Tags"><FONT size=2></FONT></A>&nbsp;</P>
+<P align=center><IMG style="HEIGHT: 123px; WIDTH: 200px" border=0 src="http://identikid.com.au/images/bg/bblankets-2_3.gif" width=0 height=0></P></TD></TR>
 <TR>
-<TD vAlign=top><FONT color=#0000ff><A href="http://identikid.com.au/Bags">
-<P align=center><FONT color=#0000ff size=2>BAGS</FONT></P>
+<TD vAlign=top><FONT color=#0000ff><A href="http://identikid.com.au/pages/Cute_&amp;_Cuddly_Koalas"><FONT color=#0000ff size=2>
+<P align=center><FONT color=#0000ff size=2>PERSONALISED AUSSIE KOALAS</FONT></P></FONT>
 <P align=center></A></FONT><A href="/Mugs"><FONT size=2></FONT></A></P><FONT size=2>
-<P align=center><FONT size=2><A href="http://identikid.com.au/Bags"><IMG border=0 src="http://identikid.com.au/images/bg/BAGSWEBSITEIMAGE.png" width=229 height=147></A><A href="http://identikid.com.au/Bags"></A></FONT></P></FONT></TD>
+<P align=center><FONT size=2><A href="http://identikid.com.au/pages/Cute_&amp;_Cuddly_Koalas"><FONT size=2><IMG style="HEIGHT: 133px; WIDTH: 138px" border=0 src="/images/bg/KOALA-GREY.gif" width=172 height=169></FONT></A><A href="http://identikid.com.au/Bags"></A></FONT></P></FONT></TD>
 <TD vAlign=top><FONT color=#0000ff><A href="http://identikid.com.au/Pencil_Cases/Pencil_Cases_to_Match_your_personality">
 <P align=center><FONT color=#0000ff size=2>WETSUIT FABRIC PENCIL CASE</FONT></P>
 <P align=center></A></FONT><A href="http://identikid.com.au/Pencil_Cases/Pencil_Cases_to_Match_your_personality"><FONT size=2></FONT></A></P>
-<P align=center><FONT size=2><A href="http://identikid.com.au/Pencil_Cases"><IMG style="WIDTH: 235px; HEIGHT: 84px" border=0 src="http://identikid.com.au/images/bg/BalletPC_2.gif" width=0 height=0></A></FONT></P></TD>
+<P align=center><FONT size=2><A href="http://identikid.com.au/Pencil_Cases"><IMG style="HEIGHT: 84px; WIDTH: 235px" border=0 src="http://identikid.com.au/images/bg/BalletPC_2.gif" width=0 height=0></A></FONT></P></TD>
 <TD vAlign=top><A href="http://identikid.com.au/pages/Large_Pencil_Cases">
 <P align=center><FONT color=#0000ff size=2>JUMBO PENCIL CASES</FONT></P>
-<P align=center></A><IMG style="WIDTH: 200px; HEIGHT: 109px" src="http://identikid.com.au/images/bg/CupcakesLPC_2.gif" width=198 height=150><A href="http://identikid.com.au/Organic_Baby_Clothes"><FONT size=2></FONT></A></P></TD></TR>
+<P align=center></A><IMG style="HEIGHT: 109px; WIDTH: 200px" src="http://identikid.com.au/images/bg/CupcakesLPC_2.gif" width=198 height=150><A href="http://identikid.com.au/Organic_Baby_Clothes"><FONT size=2></FONT></A></P></TD></TR>
 <TR>
-<TD vAlign=top><FONT color=#0000ff><FONT color=#0000ff size=2><A href="http://identikid.com.au/pages/Cute_&amp;_Cuddly_Koalas">
-<P align=center><FONT color=#0000ff size=2>PERSONALISED AUSSIE KOALAS </FONT></P>
-<P align=center></A></FONT><FONT color=#0000ff size=2><A href="http://identikid.com.au/pages/Cute_&amp;_Cuddly_Koalas"><FONT size=2><IMG style="WIDTH: 138px; HEIGHT: 133px" border=0 src="/images/bg/KOALA-GREY.gif" width=172 height=169></FONT></A></FONT></P>
-<P align=center><FONT color=#0000ff size=2>(CUDDLY LITTLE CUTIES)</FONT></FONT></P></TD>
+<TD vAlign=top><FONT color=#0000ff><FONT color=#0000ff size=2>
+<P align=center><FONT color=#0000ff size=2></FONT></FONT></FONT>&nbsp;</P></TD>
 <TD><FONT color=#0000ff><A href="http://identikid.com.au/Awesome_Aprons">
 <P align=center><FONT color=#0000ff size=2>PERSONALISED APRONS</FONT></P>
 <P align=center><FONT size=2></FONT></A></FONT><A href="/pages/Aprons_Title_Page"></A></P>
-<P align=center><A href="http://identikid.com.au/Awesome_Aprons"><FONT size=2><IMG style="WIDTH: 245px; HEIGHT: 128px" border=0 src="/images/bg/template-take-2.gif" width=0 height=0></FONT></A></P>
+<P align=center><A href="http://identikid.com.au/Awesome_Aprons"><FONT size=2><IMG style="HEIGHT: 128px; WIDTH: 245px" border=0 src="/images/bg/template-take-2.gif" width=0 height=0></FONT></A></P>
 <P align=center><FONT size=2></FONT>&nbsp;</P></TD>
 <TD vAlign=top><FONT size=2><A href="http://identikid.com.au/Organic_Baby_Clothes">
-<P align=center><FONT size=2></FONT><FONT color=#0000ff>PERSONALISED BABY CLOTHES</FONT></P>
-<P align=center></A><IMG style="WIDTH: 200px; HEIGHT: 123px" border=0 src="http://identikid.com.au/images/bg/bblankets-2_3.gif" width=0 height=0></FONT></P></TD></TR>
+<P align=center><FONT color=#0000ff></FONT>&nbsp;</P>
+<P align=center></A></FONT>&nbsp;</P></TD></TR>
 <TR>
 <TD><FONT size=4><A href="http://identikid.com.au/Organic_Baby_Clothes">
 <P align=center></A></FONT><A href="http://identikid.com.au/Organic_Baby_Clothes"></A></P></TD>
