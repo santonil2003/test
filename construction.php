5fac7909c549df1d0600890dabe9c15bedb4fdd9
sanil <santonil2004@yahoo.com>
2014-12-05 22:28:34 +1100

    under construction page

---
+++ construction.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Under Construction</title>
+	</head>
+	<body style='background:black;'>
+		<table width="100%">
+			<tr>
+				<td align="center" valgin="middle">
+					<img src='http://<?php echo $_SERVER['HTTP_HOST']?>/comming-soon.png'>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
