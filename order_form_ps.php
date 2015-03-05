66ee3664c7ac2f047cfc177e47df2df5053c83f8
sanil <santonil2004@yahoo.com>
2015-03-05 22:24:53 +1100

    credit card payment removed

--- order_form_ps.php
+++ order_form_ps.php
@@ -1348,11 +1348,12 @@ if($totalprice>0){
 	if($_COOKIE["currency"]==1){?>
                                     <select name="paymentmeth" class="ordertext" id="paymentmeth" onChange="disableStuff();">
                                       <option value="0" selected>Choose...</option>
-                                      <option value="1"<? if($_POST['paymentmeth']==1){?> selected<? }?>>Pay by credit card Now</option>
-                                      <!--<option value="4"<? if($_POST['paymentmeth']==4){?> selected<? }?>>Direct deposit by internet banking</option>
+                                      <!-- <option value="1"<? if($_POST['paymentmeth']==1){?> selected<? }?>>Pay by credit card Now</option>
+                                     <option value="4"<? if($_POST['paymentmeth']==4){?> selected<? }?>>Direct deposit by internet banking</option>
                                       <option value="5"<? if($_POST['paymentmeth']==5){?> selected<? }?>>Pay by credit card over the phone-Call during business hours</option>-->
 									  <option value="8"<? if($_POST['paymentmeth']==8){?> selected<? }?>>Pay by paypal NOW </option>
                                     </select> 
+        <span style="font-size: 10px;"><i>Note:you can pay by paypal with a credit card . No paypal account required.</i></span>
                                     <?
 	}else{
 		?>
