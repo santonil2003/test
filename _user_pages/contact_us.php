fc168e4d198cb6153ba69c8aafb6ad0d7cbc0d38
centos <santonil2004@hotmail.com>
2015-05-22 08:34:12 -0400

    user page

--- _user_pages/contact_us.php
+++ _user_pages/contact_us.php
@@ -1,8 +1,8 @@
-<TABLE border=0 cellSpacing=0 cellPadding=0 width=890>
+<TABLE cellSpacing=0 cellPadding=0 width=890 border=0>
 <TBODY>
 <TR>
 <TD>
-<TABLE border=0 cellSpacing=0 cellPadding=0 width=800>
+<TABLE cellSpacing=0 cellPadding=0 width=800 border=0>
 <TBODY>
 <TR>
 <TD vAlign=top width=59><IMG border=0 alt=Mail src="/temp/images/gen/pic_envelope_transp_bg.gif" width=59 height=43> 
@@ -17,18 +17,18 @@
 <TD vAlign=top><IMG border=0 alt=Phone src="/temp/images/gen/pic_phone_transp_bg.gif" width=56 height=42> 
 <DIV></DIV></TD>
 <TD vAlign=top>
-<P><STRONG>Phone</STRONG><BR>Australia:&nbsp; 1300 133 949 International Phone: +61 2&nbsp;69 713255 .</P>
-<P>Office hours Vary.&nbsp;&nbsp; Please leave a message and we will call within 24 hours</P></TD></TR>
+<P><STRONG>Phone</STRONG><BR>Australia:&nbsp; 1300 133 949 Office hours Vary.&nbsp;&nbsp; Please leave a message and we will call within 24 hours</P></TD></TR>
 <TR>
-<TD vAlign=top><IMG border=0 alt=Fax src="/temp/images/gen/pic_fax_transp_bg.gif" width=57 height=41> 
+<TD vAlign=top>&nbsp;
 <DIV></DIV></TD>
-<TD vAlign=top><STRONG>Fax</STRONG><BR>Australia: 1300 551 578 
+<TD vAlign=top>
 <P>
 <P></P></TD></TR>
 <TR>
 <TD vAlign=top><IMG border=0 alt=Email src="/temp/images/gen/pic_email_transp_bg.gif" width=56 height=37></TD>
-<TD vAlign=top><STRONG>Email</STRONG><BR>General Enquiries: <A href="mailto:info@identikid.com.au">info@identikid.com.au</A><BR>Management: <A href="mailto:anne@identikid.com.au">anne@identikid.com.au</A><BR>Accounts &amp; Fundraising Payment Enquiries: <A href="mailto:accounts@identikid.com.au">accounts@identikid.com.au</A><BR>Advertising/Link Enquiries: <A href="mailto:admin@identikid.com.au">admin@identikid.com.au</A><BR>Feedback: <A href="Feedback">click here</A><BR>
-<P></P></TD></TR>
+<TD vAlign=top>
+<P><STRONG>Email</STRONG><BR>General Enquiries: <A href="mailto:info@identikid.com.au">info@identikid.com.au</A><BR></P>
+<P>&nbsp;</P></TD></TR>
 <TR>
 <TD vAlign=top width=57></TD>
 <TD vAlign=top>
@@ -40,11 +40,10 @@
 <TR>
 <TD vAlign=top><BR><IMG alt=Brochure src="/temp/images/gen/spacer.gif" width=30 height=50></TD>
 <TD>
-<P><STRONG>Would you like a brochure?</STRONG><BR>
-<P><BR><STRONG>Option 2:</STRONG> <A class=type1 href="Please_Mail_Me_a_Brochure">Click here</A> to have a brochure mailed to you. 
-<P></P></TD></TR>
+<P>&nbsp;
+<P><BR>&nbsp;</P></TD></TR>
 <TR vAlign=top>
 <TD>&nbsp;</TD>
-<TD><BR><BR><STRONG>Website Design By</STRONG> 
-<P>Echidna Web Design 
-<P><A href="http://www.echidnaweb.com.au/" target=_blank><IMG border=0 alt="Visit Echidna Web Design" src="/temp/images/gen/logo_echidna_long.gif" width=187 height=27></A> </P></TD></TR></TBODY></TABLE><IMG alt="" src="/temp/images/gen/spacer.gif" width=1 height=50><BR></TD></TR></TBODY></TABLE>
+<TD>
+<P>&nbsp;
+<P><A href="http://www.echidnaweb.com.au/" target=_blank></A>&nbsp;</P></TD></TR></TBODY></TABLE><IMG alt="" src="/temp/images/gen/spacer.gif" width=1 height=50><BR></TD></TR></TBODY></TABLE>
