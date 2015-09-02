2d71c0d1e96b68c359f52923967e4eb1a7ace84e
centos <santonil2004@hotmail.com>
2015-09-02 06:22:52 -0400

    user pages

--- _user_pages/payment_information_and_details.php
+++ _user_pages/payment_information_and_details.php
@@ -1,11 +1,14 @@
-<P>We give&nbsp; you various payment options</P>
+<P>We give&nbsp; you various payment options as follows.</P>
 <P>1. <STRONG>Pay by direct debit - deposit within 48 hours the amount including correct postage to our bank account below</STRONG></P>
+<P><STRONG><FONT color=#2b00ac>Please Note our bank details have changed , please ensure our bank account details are update as below.</FONT></STRONG></P>
 <P>BSB&nbsp; 633000</P>
 <P>ACCT 155248057</P>
 <P>NAME of ACCT&nbsp; WALDEE PTY LTD</P>
 <P>REFERENCE Use your invoice NO (on your order) and the Customer details Surname so we can match your order without delay. Allow up to 2-3 business days for funds to appear in our bank account</P>
 <P>2.<STRONG> Paypal - Shop from our site and link to paypal</STRONG> for payment at finish of order. Please advise if you have any issue at payment stage and we will reissue the paypal invoice for payment. You can you use your paypal account or your credit card if you are not a paypal member.</P>
 <P>3. <STRONG>Credit cards</STRONG> - We accept Visa and MasterCard.</P>
+<P><FONT color=#d70000>Any issues with payment advise by Email&nbsp; </FONT><A href="mailto:info@identikid.com.au"><FONT color=#d70000>info@identikid.com.au</FONT></A><FONT color=#d70000> with your invoice no or surname.&nbsp; We can&nbsp;forward you a payment link which we will send on email if you want to pay by paypal or Credit card.</FONT></P>
+<P>Terms and Conditions and Shipping details can be found on our website for any further info.</P>
 <P>&nbsp;</P>
 <P>&nbsp;</P>
 <P>&nbsp;</P>
