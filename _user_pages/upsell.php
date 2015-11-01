dcebd5d54b4d1c497eddfa70a4e1cf2e26204354
centos <santonil2004@hotmail.com>
2015-11-01 01:50:22 -0500

    user pages added

--- _user_pages/upsell.php
+++ _user_pages/upsell.php
@@ -1,7 +1,7 @@
 <H2 align=center><FONT size=3><FONT color=#d72b2b size=4>
-<P align=center><STRONG>ORDERS DISPATCHED IN 5 BUSINESS DAYS CURRENTLY. </STRONG></P>
+<P align=center><STRONG>ORDERS DISPATCHED IN&nbsp;7-10 BUSINESS DAYS CURRENTLY. </STRONG></P>
 <P align=center><FONT size=5>PLEASE PAY BY PAYPAL OR DIRECT DEBIT</FONT> </P>
-<P align=center><FONT size=3>AS OUR CREDIT CARD FACILITY IS CURRENTLY UNDER MAINTENANCE AND WILL NOT PROCESS. YOU CAN USE A CREDIT CARD BY CHOOSING PAYPAL ALSO AND DO NOT REQUIRE A PAYPAL ACCOUNT. ANY PAYMENT ISSUE DO NOT RESUBMIT ORDER JUST SEND A EMAIL TO</FONT> <A href="mailto:info@identikid.com.au">info@identikid.com.au</A>&nbsp;<FONT size=3>AND WE WILL SEND A LINK TO ALLOW PAYMENT.</FONT></P>
+<P align=center><FONT size=3>AS OUR CREDIT CARD FACILITY IS CURRENTLY UNDER MAINTENANCE. YOU CAN USE A CREDIT CARD BY CHOOSING PAYPAL ALSO AND DO NOT REQUIRE A PAYPAL ACCOUNT. ANY PAYMENT ISSUE DO NOT RESUBMIT ORDER JUST SEND A EMAIL TO</FONT> <A href="mailto:info@identikid.com.au">info@identikid.com.au</A>&nbsp;<FONT size=3>AND WE WILL SEND A LINK TO ALLOW PAYMENT.</FONT></P>
 <P align=center>ALLOW NORMAL POSTAGE TIMES FROM NSW. </P>
 <P align=center>PAYMENT DETAILS&nbsp;WILL BE SENT BY EMAIL CONFIRMATION.</P>
 <P align=center>&nbsp;If you do not receive a email let us know by contacting <A href="mailto:info@identikid.com.au">info@identikid.com.au</A> as some emails get caught in junk/spam filters...</P>
