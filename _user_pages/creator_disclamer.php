2b61a59aaa1a57d5e0706cdc188e57dd457b02ed
centos <santonil2004@hotmail.com>
2015-12-03 06:45:51 -0500

    user page

--- _user_pages/creator_disclamer.php
+++ _user_pages/creator_disclamer.php
@@ -1,4 +1,4 @@
 <P align=center><STRONG>SEE <A href="http://identikid.com.au/Home/Shipping_information">SHIPPING</A> FOR CURRENT TURNAROUND TIMES. ALLOW 5-7 BUSINESS DAYS CURRENTLY.</STRONG></P>
 <P align=center><STRONG>...ORDERS MUST BE PLACED&nbsp;ON THE INTERNET FOR A SPEEDY DISPATCH.</STRONG></P>
-<P align=center><STRONG>Please Choose either Direct DEBIT or paypal OUR <FONT color=#ff0056>Credit Card gateway is currently having maintenance.</FONT></STRONG></P>
+<P align=center><STRONG>Please Choose either Direct DEBIT or paypal.</STRONG></P>
 <P align=center><STRONG></STRONG>&nbsp;</P>
